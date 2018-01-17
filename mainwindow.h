8120c9f2cb36526cdc500c0c0615dbbd0d6de3b3
Madjita <Xok-s@yandex.ru>
2018-01-17 11:23:31 +0700

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -23,7 +23,9 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-     QStringList fileNames,fileNames_S_R,fileNames_XP_XS_XW,fileNames_C_Z,fileNames_BQ,fileNames_DA_DD,fileNames_findMSWord;
+     QStringList fileNames,fileNames_S_R,fileNames_XP_XS_XW_X,fileNames_C_Z,fileNames_BQ,fileNames_DA,fileNames_DD,fileNames_findMSWord;
+
+     QStringList fileNames_U , fileNames_L,fileNames_TV;
 
      MYWORD* word ;
 
@@ -60,6 +62,14 @@ private slots:
     void on_pushButton_7_clicked();
 
     void on_pushButton_6_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_pushButton_9_clicked();
+
+    void on_pushButton_10_clicked();
+
+    void on_pushButton_11_clicked();
 
 private:
     Ui::MainWindow *ui;
